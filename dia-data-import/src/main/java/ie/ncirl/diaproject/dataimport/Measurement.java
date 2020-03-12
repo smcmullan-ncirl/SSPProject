1a82c7899dd5397789088aa566f10be73e35fcdf
Stephen McMullan <x19139497@student.ncirl.ie>
2020-03-12 18:27:40 +0000

    JSON mapper and unit tests (not working)

--- dia-data-import/src/main/java/ie/ncirl/diaproject/dataimport/Measurement.java
+++ dia-data-import/src/main/java/ie/ncirl/diaproject/dataimport/Measurement.java
@@ -1,66 +1,71 @@
 package ie.ncirl.diaproject.dataimport;
 
-import com.fasterxml.jackson.annotation.JsonAlias;
-import com.fasterxml.jackson.annotation.JsonUnwrapped;
-
 public class Measurement {
-    @JsonUnwrapped public Task task;
-    @JsonUnwrapped public Parameters parameters;
-    //public String success;
-    //public String timestamp;
-    //@JsonUnwrapped @JsonAlias({"device_properties"}) public DeviceProperties deviceProperties;
-    //@JsonUnwrapped public Values values;
-    //public String type;
-    //public String id;
+    public Task task;
+    public Parameters parameters;
+    public String success;
+    public String timestamp;
+    public DeviceProperties device_properties;
+    public Values values;
+    public String type;
+    public String id;
+
+    public Task getTask() {
+        return task;
+    }
+
+    public void setTask(Task task) {
+        this.task = task;
+    }
 
     public static class Task {
         public String count;
         public String filter;
-        @JsonUnwrapped @JsonAlias({"parameters"}) public TaskParameters taskParams;
+        public TaskParameters parameters;
         public String created;
-        @JsonAlias({"start_time"}) public String startTime;
-        @JsonAlias({"interval_sec"}) public String intervalSec;
+        public String start_time;
+        public String interval_sec;
         public String priority;
         public String tag;
-        @JsonAlias({"end_time"}) public String endTime;
+        public String end_time;
         public String type;
         public String id;
     }
 
     public static class TaskParameters {
-        @JsonAlias({"location_update_distance"}) public String locationUpdateDistance;
+        public String location_update_distance;
         public String target;
-        @JsonAlias({"trigger_location_update"}) public String triggerLocationUpdate;
+        public String trigger_location_update;
     }
 
     public static class Parameters {
         public String count;
         public String target;
-        @JsonAlias({"parameters"}) public String params;
-        //@JsonAlias({"start_time"}) public String startTime;
-        //@JsonAlias({"interval_sec"}) public String intervalSec;
-        //@JsonAlias({"ping_timeout_sec"}) public String pingTimeoutSec;
-        //public String priority;
-        //@JsonAlias({"packet_size_byte"}) public String packetSizeByte;
-        //@JsonAlias({"ping_exe"}) public String pingExe;
-        //public String key;
-        //public String type;
-        //@JsonAlias({"end_time"}) public String endTime;
+        public String parameters;
+        public String start_time;
+        public String interval_sec;
+        public String ping_timeout_sec;
+        public String priority;
+        public String packet_size_byte;
+        public String ping_exe;
+        public String key;
+        public String type;
+        public String end_time;
     }
 
     public static class DeviceProperties {
-        @JsonAlias({"battery_level"}) public String batteryLevel;
-        @JsonAlias({"cell_info"}) public String cellInfo;
+        public String battery_level;
+        public String cell_info;
         public String timestamp;
-        @JsonAlias({"network_type"}) public String networkType;
-        @JsonAlias({"os_version"}) public String osVersion;
-        @JsonUnwrapped @JsonAlias({"device_info"})  public DeviceInfo deviceInfo;
+        public String network_type;
+        public String os_version;
+        public DeviceInfo device_info;
         public String carrier;
-        @JsonUnwrapped public Location location;
+        public Location location;
         public String rssi;
-        @JsonAlias({"app_version"}) public String appVersion;
-        @JsonAlias({"location_type"}) public String locationType;
-        @JsonAlias({"is_battery_charging"}) public String isBatteryCharging;
+        public String app_version;
+        public String location_type;
+        public String is_battery_charging;
     }
 
     public static class DeviceInfo {
@@ -75,15 +80,15 @@ public class Measurement {
     }
 
     public static class Values {
-        @JsonAlias({"packets_sent"}) public String packetsSent;
-        @JsonAlias({"packet_loss"}) public String packetLoss;
-        @JsonAlias({"mean_rtt_ms"}) public String meanRttMs;
-        @JsonAlias({"CalledByLocation"}) public String calledByLocation;
-        @JsonAlias({"MeasurementLatitude"}) public String measurementLatitude;
-        @JsonAlias({"max_rtt_ms"}) public String maxRttMs;
-        @JsonAlias({"target_ip"}) public String targetIp;
-        @JsonAlias({"stddev_rtt_ms"}) public String stddevRttMs;
-        @JsonAlias({"min_rtt_ms"}) public String minRttMs;
-        @JsonAlias({"MeasurementLongitude"}) public String measurementLongitude;
+        public String packets_sent;
+        public String packet_loss;
+        public String mean_rtt_ms;
+        public String CalledByLocation;
+        public String MeasurementLatitude;
+        public String max_rtt_ms;
+        public String target_ip;
+        public String stddev_rtt_ms;
+        public String min_rtt_ms;
+        public String MeasurementLongitude;
     }
 }
