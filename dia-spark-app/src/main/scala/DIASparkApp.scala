c9cc1940c564aa58f3e2278eae66c4793b5ff0e9
Stephen McMullan <x19139497@student.ncirl.ie>
2020-03-25 23:00:30 +0000

    Started parsing the JSON and creating a DF

--- dia-spark-app/src/main/scala/DIASparkApp.scala
+++ dia-spark-app/src/main/scala/DIASparkApp.scala
@@ -1,9 +1,11 @@
 // https://spark.apache.org/docs/latest/structured-streaming-kafka-integration.html
 // https://spark.apache.org/docs/latest/streaming-kafka-0-10-integration.html
+// https://docs.databricks.com/spark/latest/dataframes-datasets/complex-nested-data.html
 
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.streaming.OutputMode
+import org.apache.spark.sql.functions.from_json
+import org.apache.spark.sql.types.{StringType, StructField, StructType}
 
 object DIASparkApp {
   def main(args: Array[String]): Unit = {
@@ -18,22 +20,57 @@ object DIASparkApp {
       .getOrCreate
 
     val df = spark
-      .readStream
+      .read
       .format("kafka")
       .option("kafka.bootstrap.servers", "localhost:9092")
       .option("subscribe", "ping")
-      .load
+      .option("startingOffsets", "earliest")
+      .load()
 
-    df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
+    val taskParametersSchema = StructType(Seq(
+    ))
 
-    val query = df
-      .writeStream
-      .outputMode(OutputMode.Append)
-      .format("console")
-      .option("checkpointLocation", "/tmp")
-      .start
+    val taskSchema = StructType(Seq(
+      StructField("count", StringType, true),
+      StructField("filter", StringType, true),
+      StructField("parameters", taskParametersSchema, true),
+      StructField("created", StringType, true),
+      StructField("start_time", StringType, true),
+      StructField("interval_sec", StringType, true),
+      StructField("priority", StringType, true),
+      StructField("tag", StringType, true),
+      StructField("end_time", StringType, true),
+      StructField("type", StringType, true),
+      StructField("id", StringType, true)
+    ))
 
-    query.awaitTermination
+    val parametersSchema = StructType(Seq(
+    ))
+
+    val devicePropertiesSchema = StructType(Seq(
+    ))
+
+    val valuesSchema = StructType(Seq(
+    ))
+
+    val measurementSchema = StructType(Seq(
+      StructField("task", taskSchema, true),
+      StructField("parameters", parametersSchema, true),
+      StructField("success", StringType, true),
+      StructField("timestamp", StringType, true),
+      StructField("device_properties", devicePropertiesSchema, true),
+      StructField("values", valuesSchema, true),
+      StructField("type", StringType, true),
+      StructField("id", StringType, true)
+    ))
+
+    import spark.implicits._
+
+    val df2 = df.selectExpr("CAST(value AS STRING) AS jsonString")
+      .select(from_json($"jsonString", schema=measurementSchema) as "measurement")
+      .select("measurement.*")
+
+    df2.show(false)
 
     spark.close
   }
