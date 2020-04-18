657a02c8b7357b5458d5dcf5595420306ef6a106
Stephen McMullan <x19139497@student.ncirl.ie>
2020-04-18 12:04:54 +0100

    Fixed unit test

--- dia-data-import/src/test/java/ie/ncirl/diaproject/dataimport/PingMeasurementTest.java
+++ dia-data-import/src/test/java/ie/ncirl/diaproject/dataimport/PingMeasurementTest.java
@@ -8,31 +8,25 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.io.TempDir;
 
-import java.io.File;
 import java.io.InputStream;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 public class PingMeasurementTest {
     private static ObjectMapper objectMapper = null;
-    private static File csvFile = null;
-
-    @TempDir
-    static File tempDir;
 
     @BeforeAll
     static void init() {
         objectMapper = new ObjectMapper();
-        csvFile = new File(tempDir, "output.csv");
     }
 
     @Test
     @DisplayName("Parse measurement JSON")
     void parseMeasurementJSON() {
         assertDoesNotThrow(() -> {
-            InputStream measurementFile = getClass().getResourceAsStream("test_measurement.json");
+            ClassLoader classLoader = getClass().getClassLoader();
+            InputStream measurementFile = classLoader.getResource("test_measurement.json").openStream();
             JsonNode jsonNode = objectMapper.readTree(measurementFile);
 
             String type = jsonNode.get("type").textValue();
