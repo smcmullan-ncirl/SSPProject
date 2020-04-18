62c71497063e339e5dd7e22a64565b3e4c5678ad
Stephen McMullan <x19139497@student.ncirl.ie>
2020-04-18 19:37:09 +0100

    Spark deployment first attempt

--- dia-spark-app/src/main/scala/ie/ncirl/diaproject/dataprocess/DIASparkApp.scala
+++ dia-spark-app/src/main/scala/ie/ncirl/diaproject/dataprocess/DIASparkApp.scala
@@ -7,7 +7,7 @@ import ie.ncirl.diaproject.dataprocess.measurement.http.HttpMeasurement
 import ie.ncirl.diaproject.dataprocess.measurement.ping.PingMeasurement
 import ie.ncirl.diaproject.dataprocess.measurement.tcpthroughput.TcpThroughputMeasurement
 import org.apache.spark.SparkConf
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.sql.catalyst.ScalaReflection
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types._
@@ -20,16 +20,20 @@ object DIASparkApp {
   private val logger = LoggerFactory.getLogger(classOf[DIASparkApp])
 
   def main(args: Array[String]): Unit = {
+    // Read in the config.properties file
     val properties: Properties = new Properties()
     properties.load(Objects.requireNonNull(getClass.getResourceAsStream("/config.properties")))
 
     val kafkaServer = properties.getProperty("kafka.server")
     val kafkaTopics = properties.getProperty("kafka.topics")
 
+    // It is worth changing this property to the number of CPUs you have available across your cluster
+    val sparkPartitions = properties.getProperty("spark.partitions")
+
+    // Set the Spark cluster configuration
     val conf: SparkConf = new SparkConf()
-      .setMaster("local[*]")
       .setAppName("DIASparkApp")
-      .set("spark.sql.shuffle.partitions", "2")
+      .set("spark.sql.shuffle.partitions", sparkPartitions)
       .set("spark.debug.maxToStringFields", "100")
 
     val spark = SparkSession
@@ -49,15 +53,13 @@ object DIASparkApp {
       .selectExpr("CAST(value AS STRING) AS jsonString")
 
     if (!rawDF.isEmpty) {
-      import spark.implicits._
-
       // Create a generic Measurement schema StructType
       val measurementSchema = ScalaReflection.schemaFor[Measurement].dataType.asInstanceOf[StructType]
 
       // Create a new DataFrame containing the JSON String in one column and the parsed JSON as a StructType column
       // called measurement in the other
-      val measurementDF = rawDF.select($"jsonString",
-        from_json($"jsonString", schema = measurementSchema) as "measurement")
+      val measurementDF = rawDF.select(col("jsonString"),
+        from_json(col("jsonString"), schema = measurementSchema) as "measurement")
 
       if (!measurementDF.isEmpty) {
         // Create a new DataFrame which flattens the measurement StructType to individual columns
@@ -73,89 +75,22 @@ object DIASparkApp {
           val measurementType = iter.next().getString(0)
           logger.info(s"Processing $measurementType measurement type")
 
-          // Get the detailed Schema for the specific measurement type
-          val typedMeasurementSchema: StructType = createMeasurementSchema(measurementType)
+          // Generate a flattened dataframe with all columns for the specific measurement type
+          val explodedTypedMeasurementDF: DataFrame = genTypedMeasurementDF(explodedMeasurementDF, measurementType)
 
-          // Create a DataFrame with just the specific measurement type as rows and a single measurement column
-          // of type StructType containing the measurement details
-          val typedMeasurementDF = explodedMeasurementDF
-            .filter(col("measurement_type") === measurementType)
-            .select(from_json($"jsonString", schema = typedMeasurementSchema) as "measurement")
+          genMeasurementAggs(explodedTypedMeasurementDF)
 
-          // Create a new DataFrame which flattens the measurement StructType to individual columns
-          val explodedTypedMeasurementDF = JSONUtils.flattenDataFrame(typedMeasurementDF)
+          genMeasurementTypeAggs(measurementType)
 
-          // Start doing some interesting aggregations here
-          explodedTypedMeasurementDF
-            .groupBy($"measurement_device_properties_carrier",
-              $"measurement_device_properties_network_type")
-            .count()
-            .orderBy($"measurement_device_properties_carrier",
-              $"measurement_device_properties_network_type")
-            .withColumnRenamed("measurement_device_properties_carrier", "Carrier")
-            .withColumnRenamed("measurement_device_properties_network_type", "Network Type")
-            .show(false);
-
-          explodedTypedMeasurementDF
-            .groupBy($"measurement_device_properties_device_info_manufacturer",
-              $"measurement_device_properties_device_info_model")
-            .count()
-            .orderBy($"measurement_device_properties_device_info_manufacturer",
-              $"measurement_device_properties_device_info_model")
-            .withColumnRenamed("measurement_device_properties_device_info_manufacturer", "Manufacturer")
-            .withColumnRenamed("measurement_device_properties_device_info_model", "Model")
-            .show(false);
-
-          explodedTypedMeasurementDF
-            .agg(max("measurement_device_properties_battery_level"))
-            .withColumnRenamed("max(measurement_device_properties_battery_level)", "Max Battery Level")
-            .show(false)
-
-          explodedTypedMeasurementDF
-            .agg(min("measurement_device_properties_battery_level"))
-            .withColumnRenamed("min(measurement_device_properties_battery_level)", "Min Battery Level")
-            .show(false)
-
-          explodedTypedMeasurementDF
-            .agg(avg("measurement_device_properties_battery_level"))
-            .withColumnRenamed("avg(measurement_device_properties_battery_level)", "Avg Battery Level")
-            .show(false)
-
-          explodedTypedMeasurementDF
-            .agg(max("measurement_device_properties_rssi"))
-            .withColumnRenamed("max(measurement_device_properties_rssi)", "Max RSSI")
-            .show(false)
-
-          explodedTypedMeasurementDF
-            .agg(min("measurement_device_properties_rssi"))
-            .withColumnRenamed("min(measurement_device_properties_rssi)", "Min RSSI")
-            .show(false)
-
-          explodedTypedMeasurementDF
-            .agg(avg("measurement_device_properties_rssi"))
-            .withColumnRenamed("avg(measurement_device_properties_rssi)", "Avg RSSI")
-            .show(false)
-
-          measurementType match {
-            case "http" => {
-
-            }
-
-            case "ping" => {
-
-            }
-
-            case _ =>
-          }
-
-          explodedTypedMeasurementDF
-            .repartition(1)
-            .write
-            .format("csv")
-            .mode("overwrite")
-            .option("sep", "\t")
-            .option("header", "true")
-            .save(s"${measurementType}_output.tsv")
+          // DEBUG: Write out the data frame to a measurement specific TSV file
+//          explodedTypedMeasurementDF
+//            .repartition(1)
+//            .write
+//            .format("csv")
+//            .mode("overwrite")
+//            .option("sep", "\t")
+//            .option("header", "true")
+//            .save(s"${measurementType}_output.tsv")
         }
       }
     }
@@ -163,7 +98,22 @@ object DIASparkApp {
     spark.close
   }
 
-  def createMeasurementSchema(measurementType: String): StructType = {
+  private def genTypedMeasurementDF(explodedMeasurementDF: DataFrame, measurementType: String) = {
+    // Get the detailed Schema for the specific measurement type
+    val typedMeasurementSchema: StructType = createMeasurementSchema(measurementType)
+
+    // Create a DataFrame with just the specific measurement type as rows and a single measurement column
+    // of type StructType containing the measurement details
+    val typedMeasurementDF = explodedMeasurementDF
+      .filter(col("measurement_type") === measurementType)
+      .select(from_json(col("jsonString"), schema = typedMeasurementSchema) as "measurement")
+
+    // Create a new DataFrame which flattens the measurement StructType to individual columns
+    val explodedTypedMeasurementDF = JSONUtils.flattenDataFrame(typedMeasurementDF)
+    explodedTypedMeasurementDF
+  }
+
+  private def createMeasurementSchema(measurementType: String): StructType = {
     var measurementSchema: StructType = null
 
     measurementType match {
@@ -199,6 +149,91 @@ object DIASparkApp {
     }
 
     measurementSchema
+  }
+
+  private def genMeasurementAggs(explodedTypedMeasurementDF: DataFrame): Unit = {
+    // Start doing some interesting aggregations here - these are generic to all measurement types
+    explodedTypedMeasurementDF
+      .groupBy(col("measurement_device_properties_carrier"),
+        col("measurement_device_properties_network_type"))
+      .count()
+      .orderBy(col("measurement_device_properties_carrier"),
+        col("measurement_device_properties_network_type"))
+      .withColumnRenamed("measurement_device_properties_carrier", "Carrier")
+      .withColumnRenamed("measurement_device_properties_network_type", "Network Type")
+      .show(false)
+
+    explodedTypedMeasurementDF
+      .groupBy(col("measurement_device_properties_device_info_manufacturer"),
+        col("measurement_device_properties_device_info_model"))
+      .count()
+      .orderBy(col("measurement_device_properties_device_info_manufacturer"),
+        col("measurement_device_properties_device_info_model"))
+      .withColumnRenamed("measurement_device_properties_device_info_manufacturer", "Manufacturer")
+      .withColumnRenamed("measurement_device_properties_device_info_model", "Model")
+      .show(false)
+
+    explodedTypedMeasurementDF
+      .agg(max("measurement_device_properties_battery_level"))
+      .withColumnRenamed("max(measurement_device_properties_battery_level)", "Max Battery Level")
+      .show(false)
+
+    explodedTypedMeasurementDF
+      .agg(min("measurement_device_properties_battery_level"))
+      .withColumnRenamed("min(measurement_device_properties_battery_level)", "Min Battery Level")
+      .show(false)
+
+    explodedTypedMeasurementDF
+      .agg(avg("measurement_device_properties_battery_level"))
+      .withColumnRenamed("avg(measurement_device_properties_battery_level)", "Avg Battery Level")
+      .show(false)
+
+    explodedTypedMeasurementDF
+      .agg(max("measurement_device_properties_rssi"))
+      .withColumnRenamed("max(measurement_device_properties_rssi)", "Max RSSI")
+      .show(false)
+
+    explodedTypedMeasurementDF
+      .agg(min("measurement_device_properties_rssi"))
+      .withColumnRenamed("min(measurement_device_properties_rssi)", "Min RSSI")
+      .show(false)
+
+    explodedTypedMeasurementDF
+      .agg(avg("measurement_device_properties_rssi"))
+      .withColumnRenamed("avg(measurement_device_properties_rssi)", "Avg RSSI")
+      .show(false)
+  }
+
+  private def genMeasurementTypeAggs(measurementType: String): Unit = {
+    // TBD: Do some more interesing aggregations on a per measurement type basis
+    measurementType match {
+      case "ping" =>
+      case "traceroute" =>
+      case "http" =>
+      case "dns_lookup" =>
+      case "udp_burst" =>
+      case "tcpthroughput" =>
+      case "context" =>
+      case "myspeedtest_ping" =>
+      case "myspeedtestdns_lookup" =>
+      case "device_info" =>
+      case "network_info" =>
+      case "battery_info" =>
+      case "ping_test" =>
+      case "sim_info" =>
+      case "state_info" =>
+      case "usage_info" =>
+      case "rrc" =>
+      case "PageLoadTime" =>
+      case "pageloadtime" =>
+      case "video" =>
+      case "sequential" =>
+      case "quic-http" =>
+      case "cronet-http" =>
+      case "multipath_latency" =>
+      case "multipath_http" =>
+      case _ =>
+    }
   }
 
 }
