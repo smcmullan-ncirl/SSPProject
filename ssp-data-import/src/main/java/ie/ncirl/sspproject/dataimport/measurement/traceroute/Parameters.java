3ae69dbfb9988ae18587114a2a1f7f06b01b50b1
Stephen McMullan <x19139497@student.ncirl.ie>
2020-05-28 21:49:20 +0100

    Traceroute support CSV files and dependency version management

--- ssp-data-import/src/main/java/ie/ncirl/sspproject/dataimport/measurement/traceroute/Parameters.java
+++ ssp-data-import/src/main/java/ie/ncirl/sspproject/dataimport/measurement/traceroute/Parameters.java
@@ -24,16 +24,73 @@ public class Parameters extends Measurement {
 
     @Override
     public String toHdr(String sep) throws UnsupportedOperationException {
-        return null;
+        StringBuffer sb = new StringBuffer();
+        separate(sb, "p_count", sep);
+        separate(sb, "p_ping_timeout_sec", sep);
+        separate(sb, "p_parameters", sep);
+        separate(sb, "p_start_time", sep);
+        separate(sb, "p_ping_interval_sec", sep);
+        separate(sb, "p_interval_sec", sep);
+        separate(sb, "p_pings_per_hop", sep);
+        separate(sb, "p_priority", sep);
+        separate(sb, "p_packet_size_byte", sep);
+        separate(sb, "p_ping_exe", sep);
+        separate(sb, "p_key", sep);
+        separate(sb, "p_target", sep);
+        separate(sb, "p_max_hop_count", sep);
+        separate(sb, "p_type", sep);
+        separate(sb, "p_end_time", sep);
+        separate(sb, "p_context_interval_sec", sep);
+        separate(sb, "p_pre_condition", sep);
+        separate(sb, "p_parallel_probe_num", NO_SEP);
+        return(sb.toString());
     }
 
     @Override
     public String toCsv(String nullValue, String quote, String sep) throws UnsupportedOperationException {
-        return null;
+        StringBuffer sb = new StringBuffer();
+        quoteAndSeparate(sb, count, quote, sep);
+        quoteAndSeparate(sb, ping_timeout_sec, quote, sep);
+        quoteAndSeparate(sb, parameters, quote, sep);
+        quoteAndSeparate(sb, start_time, quote, sep);
+        quoteAndSeparate(sb, ping_interval_sec, quote, sep);
+        quoteAndSeparate(sb, interval_sec, quote, sep);
+        quoteAndSeparate(sb, pings_per_hop, quote, sep);
+        quoteAndSeparate(sb, priority, quote, sep);
+        quoteAndSeparate(sb, packet_size_byte, quote, sep);
+        quoteAndSeparate(sb, ping_exe, quote, sep);
+        quoteAndSeparate(sb, key, quote, sep);
+        quoteAndSeparate(sb, target, quote, sep);
+        quoteAndSeparate(sb, max_hop_count, quote, sep);
+        quoteAndSeparate(sb, type, quote, sep);
+        quoteAndSeparate(sb, end_time, quote, sep);
+        quoteAndSeparate(sb, context_interval_sec, quote, sep);
+        quoteAndSeparate(sb, pre_condition, quote, sep);
+        quoteAndSeparate(sb, parallel_probe_num, quote, NO_SEP);
+        return(sb.toString());
     }
 
     @Override
     public String toNullCsv(String nullValue, String sep) throws UnsupportedOperationException {
-        return null;
+        StringBuffer sb = new StringBuffer();
+        separate(sb, nullValue, sep); // count
+        separate(sb, nullValue, sep); // ping_timeout_sec
+        separate(sb, nullValue, sep); // parameters
+        separate(sb, nullValue, sep); // start_time
+        separate(sb, nullValue, sep); // ping_interval_sec
+        separate(sb, nullValue, sep); // interval_sec
+        separate(sb, nullValue, sep); // pings_per_hop
+        separate(sb, nullValue, sep); // priority
+        separate(sb, nullValue, sep); // packet_size_byte
+        separate(sb, nullValue, sep); // ping_exe
+        separate(sb, nullValue, sep); // key
+        separate(sb, nullValue, sep); // target
+        separate(sb, nullValue, sep); // max_hop_count
+        separate(sb, nullValue, sep); // type
+        separate(sb, nullValue, sep); // end_time
+        separate(sb, nullValue, sep); // context_interval_sec
+        separate(sb, nullValue, sep); // pre_condition
+        separate(sb, nullValue, NO_SEP); // parallel_probe_num
+        return(sb.toString());
     }
 }
