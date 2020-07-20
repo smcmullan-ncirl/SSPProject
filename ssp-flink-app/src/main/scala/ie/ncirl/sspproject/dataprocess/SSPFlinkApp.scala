56087e8362c9394c7d203e2ce53520a4a9717832
Stephen McMullan <x19139497@student.ncirl.ie>
2020-07-20 22:35:32 +0100

    Object mapping in Flink

--- ssp-flink-app/src/main/scala/ie/ncirl/sspproject/dataprocess/SSPFlinkApp.scala
+++ ssp-flink-app/src/main/scala/ie/ncirl/sspproject/dataprocess/SSPFlinkApp.scala
@@ -2,17 +2,20 @@ package ie.ncirl.sspproject.dataprocess
 
 import java.util.{Objects, Properties}
 
-import org.apache.flink.api.common.functions.RuntimeContext
-import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.node.ObjectNode
-import org.apache.flink.streaming.api.datastream.DataStreamSource
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.scala.DefaultScalaModule
+import org.apache.flink.api.common.functions.{MapFunction, ReduceFunction, RuntimeContext}
+import org.apache.flink.api.common.serialization.SimpleStringSchema
+import org.apache.flink.streaming.api.datastream.{DataStreamSource, SingleOutputStreamOperator}
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment
+import org.apache.flink.streaming.api.windowing.time.Time
 import org.apache.flink.streaming.connectors.elasticsearch.{ElasticsearchSinkFunction, RequestIndexer}
 import org.apache.flink.streaming.connectors.elasticsearch7.ElasticsearchSink
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer
-import org.apache.flink.streaming.util.serialization.JSONKeyValueDeserializationSchema
 import org.apache.http.HttpHost
 import org.elasticsearch.action.index.IndexRequest
 import org.elasticsearch.client.Requests
+import org.elasticsearch.common.xcontent.XContentType
 
 class SSPFlinkApp {
 }
@@ -33,28 +36,47 @@ object SSPFlinkApp {
     val kafkaProperties: Properties = new Properties()
     kafkaProperties.setProperty("bootstrap.servers", kafkaServer)
 
-    val kafkaConsumer: FlinkKafkaConsumer[ObjectNode] = new FlinkKafkaConsumer(
+    val kafkaConsumer: FlinkKafkaConsumer[String] = new FlinkKafkaConsumer(
       kafkaTopics,
-      new JSONKeyValueDeserializationSchema(false),
+      new SimpleStringSchema(),
       kafkaProperties)
 
     val env = StreamExecutionEnvironment.getExecutionEnvironment
 
-    val messageStream: DataStreamSource[ObjectNode] = env.addSource(kafkaConsumer)
+    val messageStream: DataStreamSource[String] = env.addSource(kafkaConsumer)
+
+    val telecomRecordStream: SingleOutputStreamOperator[TelecomRecord] = messageStream.map(
+      new MapFunction[String, TelecomRecord] {
+        override def map(jsonString: String): TelecomRecord = {
+          val mapper: ObjectMapper = new ObjectMapper()
+          mapper.registerModule(DefaultScalaModule)
+          mapper.readValue(jsonString, classOf[TelecomRecord])
+        }
+      }
+    )
+
+    telecomRecordStream
+      .timeWindowAll(Time.seconds(5))
+      .reduce(new ReduceFunction[TelecomRecord] {
+        override def reduce(t1: TelecomRecord, t2: TelecomRecord): TelecomRecord = {
+          t1
+        }
+      })
+      .print
 
     val httpHosts = new java.util.ArrayList[HttpHost]
     httpHosts.add(new HttpHost(esServer, esPort, "http"))
 
-    val esSinkBuilder = new ElasticsearchSink.Builder[ObjectNode](
+    val esSinkBuilder = new ElasticsearchSink.Builder[TelecomRecord](
       httpHosts,
-      new ElasticsearchSinkFunction[ObjectNode] {
-        def process(element: ObjectNode, ctx: RuntimeContext, indexer: RequestIndexer) {
-          val json = new java.util.HashMap[String, String]
-          json.put("data", element.get("value").asText())
+      new ElasticsearchSinkFunction[TelecomRecord] {
+        def process(element: TelecomRecord, ctx: RuntimeContext, indexer: RequestIndexer) {
+          val mapper: ObjectMapper = new ObjectMapper()
+          val jsonData = mapper.writeValueAsString(element)
 
           val rqst: IndexRequest = Requests.indexRequest
             .index(esIndex)
-            .source(json)
+            .source(jsonData, XContentType.JSON)
 
           indexer.add(rqst)
         }
@@ -63,9 +85,23 @@ object SSPFlinkApp {
 
     esSinkBuilder.setBulkFlushMaxActions(1)
 
-    messageStream.addSink(esSinkBuilder.build())
+    telecomRecordStream.addSink(esSinkBuilder.build())
 
     env.execute
   }
+
+  case class TelecomRecord
+  (
+    cell_id: String,
+    timestamp: Long,
+    area_code: Int,
+    calls_in: Double,
+    calls_out: Double,
+    sms_in: Double,
+    sms_out: Double,
+    internet_activity: Double
+  ) extends Serializable
 }
+
+
 
