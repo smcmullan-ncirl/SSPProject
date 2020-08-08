121c06331b2f6edb8868c010705d4d3dfeb35e74
Stephen McMullan <x19139497@student.ncirl.ie>
2020-08-08 20:27:10 +0100

    Flink working for Hourly Aggs

--- ssp-flink-app/src/main/scala/ie/ncirl/sspproject/dataprocess/SSPFlinkApp.scala
+++ ssp-flink-app/src/main/scala/ie/ncirl/sspproject/dataprocess/SSPFlinkApp.scala
@@ -4,14 +4,20 @@ import java.util.{Date, Objects, Properties}
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
-import org.apache.flink.api.common.functions.{MapFunction, ReduceFunction, RuntimeContext}
+import org.apache.flink.api.common.functions.{MapFunction, RuntimeContext}
 import org.apache.flink.api.common.serialization.SimpleStringSchema
-import org.apache.flink.streaming.api.datastream.{DataStreamSource, SingleOutputStreamOperator}
-import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment
+import org.apache.flink.api.java.tuple.Tuple
+import org.apache.flink.api.scala._
+import org.apache.flink.streaming.api.TimeCharacteristic
+import org.apache.flink.streaming.api.scala.function.ProcessWindowFunction
+import org.apache.flink.streaming.api.scala.{DataStream, StreamExecutionEnvironment}
+import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows
 import org.apache.flink.streaming.api.windowing.time.Time
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow
 import org.apache.flink.streaming.connectors.elasticsearch.{ElasticsearchSinkFunction, RequestIndexer}
 import org.apache.flink.streaming.connectors.elasticsearch7.ElasticsearchSink
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer
+import org.apache.flink.util.Collector
 import org.apache.http.HttpHost
 import org.elasticsearch.action.index.IndexRequest
 import org.elasticsearch.client.Requests
@@ -40,12 +46,14 @@ object SSPFlinkApp {
       kafkaTopics,
       new SimpleStringSchema(),
       kafkaProperties)
+    kafkaConsumer.setStartFromLatest()
 
     val env = StreamExecutionEnvironment.getExecutionEnvironment
+    env.setStreamTimeCharacteristic(TimeCharacteristic.ProcessingTime)
 
-    val messageStream: DataStreamSource[String] = env.addSource(kafkaConsumer)
+    val messageStream: DataStream[String] = env.addSource(kafkaConsumer)
 
-    val telecomRecordStream: SingleOutputStreamOperator[TelecomRecord] = messageStream.map(
+    val telecomRecordStream: DataStream[TelecomRecord] = messageStream.map(
       new MapFunction[String, TelecomRecord] {
         override def map(jsonString: String): TelecomRecord = {
           val mapper: ObjectMapper = new ObjectMapper()
@@ -55,27 +63,50 @@ object SSPFlinkApp {
       }
     )
 
-    telecomRecordStream
-      .timeWindowAll(Time.seconds(5))
-      .reduce(new ReduceFunction[TelecomRecord] {
-        override def reduce(t1: TelecomRecord, t2: TelecomRecord): TelecomRecord = {
-          t1
+    val telecomHourlyAggs = telecomRecordStream
+      .keyBy("hourly_timestamp", "area_code")
+      .window(TumblingProcessingTimeWindows.of(Time.seconds(60)))
+      .process(new ProcessWindowFunction[TelecomRecord, TelecomAgg, Tuple, TimeWindow]() {
+        override def process(key: Tuple, context: Context, elements: Iterable[TelecomRecord], out: Collector[TelecomAgg]): Unit = {
+          var total_calls_in: Double = 0
+          var total_calls_out: Double = 0
+          var total_sms_in: Double = 0
+          var total_sms_out: Double = 0
+
+          while(elements.iterator.hasNext) {
+            val rec = elements.iterator.next
+
+            total_calls_in += rec.calls_in
+            total_calls_out += rec.calls_out
+            total_sms_in += rec.sms_in
+            total_sms_out += rec.sms_out
+          }
+
+          out.collect(
+            TelecomAgg(
+              key.getField(1),
+              key.getField(2),
+              total_calls_in,
+              total_calls_out,
+              total_sms_in,
+              total_sms_out)
+          )
         }
-      })
-      .print
+      }
+      )
 
     val httpHosts = new java.util.ArrayList[HttpHost]
     httpHosts.add(new HttpHost(esServer, esPort, "http"))
 
-    val esSinkBuilder = new ElasticsearchSink.Builder[TelecomRecord](
+    val esSinkBuilderHourly = new ElasticsearchSink.Builder[TelecomAgg](
       httpHosts,
-      new ElasticsearchSinkFunction[TelecomRecord] {
-        def process(element: TelecomRecord, ctx: RuntimeContext, indexer: RequestIndexer) {
+      new ElasticsearchSinkFunction[TelecomAgg] {
+        def process(element: TelecomAgg, ctx: RuntimeContext, indexer: RequestIndexer) {
           val mapper: ObjectMapper = new ObjectMapper()
           val jsonData = mapper.writeValueAsString(element)
 
           val rqst: IndexRequest = Requests.indexRequest
-            .index(esIndex)
+            .index(esIndex + "_hourly")
             .source(jsonData, XContentType.JSON)
 
           indexer.add(rqst)
@@ -83,9 +114,11 @@ object SSPFlinkApp {
       }
     )
 
-    esSinkBuilder.setBulkFlushMaxActions(1)
+    esSinkBuilderHourly.setBulkFlushMaxActions(1)
 
-    telecomRecordStream.addSink(esSinkBuilder.build())
+    telecomHourlyAggs.addSink(esSinkBuilderHourly.build())
+
+    telecomHourlyAggs.print
 
     env.execute
   }
@@ -108,6 +141,16 @@ object SSPFlinkApp {
     weekly_timestamp: Long,
     weekly_timestamp_str: Date
   ) extends Serializable
+
+  case class TelecomAgg
+  (
+    timestamp: Long,
+    area_code: Int,
+    calls_in: Double,
+    calls_out: Double,
+    sms_in: Double,
+    sms_out: Double
+  )
 }
 
 
